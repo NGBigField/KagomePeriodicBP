62cb0865e841408450c6437f2973668480a3188e
Nir Gutman <NGBigfield@gmail.com>
2023-09-06 19:45:31 +0300

    condor update

--- scripts/condor/sender.py
+++ scripts/condor/sender.py
@@ -28,7 +28,7 @@ RESULT_KEYS_DICT = dict(
 
 ## all values:
 DEFAULT_VALS = {}
-DEFAULT_VALS['N'] = [3] #range(2, 11, 1)
+DEFAULT_VALS['N'] = [4] #range(2, 11, 1)
 DEFAULT_VALS['D'] = [2, 3, 4]
 DEFAULT_VALS['chi'] = [-1]
 DEFAULT_VALS['method'] = [1]
