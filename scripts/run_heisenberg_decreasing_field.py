ef154b3c9d8f81cc2d407b36f7a3d1228881b8e4
Nir Gutman <NGBigfield@gmail.com>
2024-04-18 13:54:03 -0400

    Update run_heisenberg_decreasing_field.py

--- scripts/run_heisenberg_decreasing_field.py
+++ scripts/run_heisenberg_decreasing_field.py
@@ -37,7 +37,7 @@ def run_single_ite(
     config.ite.interaction_hamiltonian = (hamiltonians.heisenberg_afm_with_field, field_strength, None)
 
     ## Run:
-    energy, unit_cell_out, ite_tracker, logger = full_ite(
+    energy, unit_cell, ite_tracker, logger = full_ite(
         unit_cell, config=config, common_results_name=crnt_results_name, logger=logger
     )
 
@@ -56,12 +56,12 @@ def main(
     parallel:bool = 0,
     active_bp:bool = True,
     damping:float|None = None,
-    field_strength_values = np.arange(1.5, 0, -0.1)
+    field_strength_values = np.arange(1.5, 0, -0.1).tolist()
 )->tuple[float, str]:
     
-    # unit_cell = UnitCell.load("last")
+    unit_cell = UnitCell.load("last")
     # unit_cell = UnitCell.load("2024.04.11_09.43.42_CGOP - stable -0.25")
-    unit_cell = UnitCell.random(d=d, D=D)
+    # unit_cell = UnitCell.random(d=d, D=D)
 
     ## Config:
     config = Config.derive_from_physical_dim(D)
@@ -71,8 +71,8 @@ def main(
     config.bp.parallel_msgs = parallel
     config.trunc_dim = int(4*D**2+20 * chi_factor)
     config.bp.max_swallowing_dim = int(4*D**2 * chi_factor)
-    config.bp.msg_diff_terminate = 1e-6
-    config.bp.msg_diff_good_enough = 1e-5
+    config.bp.msg_diff_terminate = 1e-5
+    config.bp.msg_diff_good_enough = 1e-4
     config.bp.times_to_deem_failure_when_diff_increases = 4
     config.bp.max_iterations = 30
     config.bp.allowed_retries = 2
@@ -83,7 +83,7 @@ def main(
     config.iterative_process.change_config_for_measurements_func = _config_at_measurement
     config.iterative_process.use_bp = active_bp
     config.ite.normalize_tensors_after_update = True
-    config.ite.time_steps = [[10**(-exp)]*15 for exp in range(1, 8, 1)]
+    config.ite.time_steps = [[10**(-exp)]*15 for exp in range(2, 6, 1)]
 
 
     logger = logs.get_logger(verbose=config.visuals.verbose, write_to_file=True, filename=results_filename)
@@ -94,13 +94,17 @@ def main(
         prog_bar.next(extra_str=f" field={field_strength}")
         crnt_results_name = results_filename+"_"+strings.formatted(i, fill="0", width=3)+f"_f={field_strength}"
 
-        unit_cell = run_single_ite(
-            unit_cell=unit_cell,
-            field_strength=field_strength, 
-            config=config,
-            crnt_results_name=crnt_results_name,
-            logger=logger
-        )
+        try:
+            unit_cell = run_single_ite(
+                unit_cell=unit_cell,
+                field_strength=field_strength, 
+                config=config,
+                crnt_results_name=crnt_results_name,
+                logger=logger
+            )
+        except Exception as e:
+            continue
+
         config.ite.time_steps = [[10**(-exp)]*15 for exp in range(4, 8, 1)]
 
     prog_bar.clear()
