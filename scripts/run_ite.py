6a7de54e3eaed6156384e4f688a753e90a39375e
Nir Gutman <NGBigfield@gmail.com>
2024-05-01 08:36:40 -0400

    Update run_ite.py

--- scripts/run_ite.py
+++ scripts/run_ite.py
@@ -37,14 +37,19 @@ def constant_global_field(delta_t:float|None)->float:
 
 def _config_at_measurement(config:Config)->Config:
     config.dims.big_lattice_size += 0
-    config.bp.msg_diff_terminate /= 1
-    config.bp.allowed_retries    += 1
+    config.bp.msg_diff_terminate /= 10
+    config.bp.allowed_retries    += 0
+
+    chi_factor = 2
+    D = config.dims.virtual_dim
+    config.trunc_dim = int((2*D**2+10) * chi_factor)
+    config.bp.max_swallowing_dim = int(2*D**2 * chi_factor)
     return config
 
 
 def main(
     D = 2,
-    N = 3,
+    N = 2,
     chi_factor : int = 1.0,
     live_plots:bool|Iterable[bool] = [0, 0, 0],
     results_filename:str = strings.time_stamp()+"_"+strings.random(4),
@@ -62,11 +67,11 @@ def main(
     config = Config.derive_from_physical_dim(D)
     config.dims.big_lattice_size = N
     config.visuals.live_plots = live_plots
-    config.trunc_dim = int((4*D**2+20) * chi_factor)
-    config.bp.max_swallowing_dim = int(4*D**2 * chi_factor)
+    config.trunc_dim = int((2*D**2+10) * chi_factor)
+    config.bp.max_swallowing_dim = int(2*D**2 * chi_factor)
     config.bp.damping = damping
     config.bp.parallel_msgs = parallel
-    config.bp.msg_diff_terminate = 1e-11
+    config.bp.msg_diff_terminate = 1e-7
     config.bp.msg_diff_good_enough = 1e-4
     config.bp.times_to_deem_failure_when_diff_increases = 4
     config.bp.max_iterations = 50
@@ -81,8 +86,8 @@ def main(
     config.ite.random_edge_order = False
     config.ite.symmetric_product_formula = True
     config.ite.always_use_lowest_energy_state = False
-    config.ite.add_gaussian_noise_fraction = 1e-8
-    config.ite.time_steps = [[10**(-exp)]*100 for exp in np.linspace(3, 4, 6)]
+    config.ite.add_gaussian_noise_fraction = 0
+    config.ite.time_steps = [[10**(-exp)]*100 for exp in np.linspace(3.0, 5.5, 6)]
     # config.ite.time_steps = [[[man*10**(-exp)]*10 for man in [5, 2, 1]] for exp in range(3, 5, 1)]
 
     # Interaction:
